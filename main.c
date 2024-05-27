bc8320a5a08358df0bea13762bf82f87aa8077c0
Momo-azangue <azanguewill@gmail.com>
2024-05-28 01:33:18 +0200

    troisième   commit

--- main.c
+++ main.c
@@ -1,30 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "PrimAdja.h"
+#include <limits.h>
+#include <stdbool.h>
 
+typedef struct Node {
+    int dest;
+    int weight;
+    struct Node* next;
+} Node;
 
+typedef struct AdjList {
+    Node* head;
+} AdjList;
 
+typedef struct Graph {
+    int V;
+    AdjList* array;
+} Graph;
 
+Node* createNode(int dest, int weight) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->dest = dest;
+    newNode->weight = weight;
+    newNode->next = NULL;
+    return newNode;
+}
 
+Graph* createGraph(int V) {
+    Graph* graph = (Graph*)malloc(sizeof(Graph));
+    graph->V = V;
+    graph->array = (AdjList*)malloc(V * sizeof(AdjList));
+    for (int i = 0; i < V; ++i) {
+        graph->array[i].head = NULL;
+    }
+    return graph;
+}
 
+void addEdge(Graph* graph, int src, int dest, int weight) {
+    Node* newNode = createNode(dest, weight);
+    newNode->next = graph->array[src].head;
+    graph->array[src].head = newNode;
 
+    newNode = createNode(src, weight);
+    newNode->next = graph->array[dest].head;
+    graph->array[dest].head = newNode;
+}
 
+int minDistance(int dist[], bool sptSet[], int V) {
+    int min = INT_MAX, min_index;
+    for (int v = 0; v < V; v++) {
+        if (sptSet[v] == false && dist[v] <= min) {
+            min = dist[v], min_index = v;
+        }
+    }
+    return min_index;
+}
+
+void printSolution(int dist[], int n) {
+    printf("Vertex \t Distance from Source\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d \t\t %d\n", i, dist[i]);
+    }
+}
+
+void dijkstra(Graph* graph, int src) {
+    int V = graph->V;
+    int dist[V];
+    bool sptSet[V];
+
+    for (int i = 0; i < V; i++) {
+        dist[i] = INT_MAX, sptSet[i] = false;
+    }
+
+    dist[src] = 0;
+
+    for (int count = 0; count < V - 1; count++) {
+        int u = minDistance(dist, sptSet, V);
+        sptSet[u] = true;
+
+        Node* temp = graph->array[u].head;
+        while (temp != NULL) {
+            int v = temp->dest;
+            if (!sptSet[v] && dist[u] != INT_MAX && dist[u] + temp->weight < dist[v]) {
+                dist[v] = dist[u] + temp->weight;
+            }
+            temp = temp->next;
+        }
+    }
+
+    printSolution(dist, V);
+}
 int main() {
 
+    int V = 9;
+    Graph* graph = createGraph(V);
+    addEdge(graph, 0, 1, 4);
+    addEdge(graph, 0, 7, 8);
+    addEdge(graph, 1, 2, 8);
+    addEdge(graph, 1, 7, 11);
+    addEdge(graph, 2, 3, 7);
+    addEdge(graph, 2, 8, 2);
+    addEdge(graph, 2, 5, 4);
+    addEdge(graph, 3, 4, 9);
+    addEdge(graph, 3, 5, 14);
+    addEdge(graph, 4, 5, 10);
+    addEdge(graph, 5, 6, 2);
+    addEdge(graph, 6, 7, 1);
+    addEdge(graph, 6, 8, 6);
+    addEdge(graph, 7, 8, 7);
 
-    int graph[V][V] = {
-            {0, 4, 0, 0, 0, 0, 0, 8, 0},
-            {4, 0, 8, 0, 0, 0, 0, 11, 0},
-            {0, 8, 0, 7, 0, 4, 0, 0, 2},
-            {0, 0, 7, 0, 9, 14, 0, 0, 0},
-            {0, 0, 0, 9, 0, 10, 0, 0, 0},
-            {0, 0, 4, 14, 10, 0, 2, 0, 0},
-            {0, 0, 0, 0, 0, 2, 0, 1, 6},
-            {8, 11, 0, 0, 0, 0, 1, 0, 7},
-            {0, 0, 2, 0, 0, 0, 6, 7, 0}
-    };
-
-    primMST(graph);
-
+    dijkstra(graph, 0);
     return 0;
 }
