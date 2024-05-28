bcee0146d3feddc088bcbf2b90fe7bfdfc369972
Momo-azangue <azanguewill@gmail.com>
2024-05-28 02:46:17 +0200

    cinquièmee    commit

--- main.c
+++ main.c
@@ -1,8 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <stdbool.h>
+#define V 9
+
+int minKey(int key[], bool mstSet[]) {
+    int min = INT_MAX, min_index;
+    for (int v = 0; v < V; v++) {
+        if (mstSet[v] == false && key[v] < min) {
+            min = key[v], min_index = v;
+        }
+    }
+    return min_index;
+}
+
+void generateDotFile(int parent[], int graph[V][V]) {
+    FILE *file = fopen("mst.dot", "w");
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+
+    fprintf(file, "graph MST {\n");
+    for (int i = 1; i < V; i++) {
+        fprintf(file, "  %d -- %d [label=\"%d\"];\n", parent[i], i, graph[i][parent[i]]);
+    }
+    fprintf(file, "}\n");
+
+    fclose(file);
+}
+
+void printMST(int parent[], int graph[V][V]) {
+    printf("Edge \tWeight\n");
+    for (int i = 1; i < V; i++) {
+        printf("%d - %d \t%d \n", parent[i], i, graph[i][parent[i]]);
+    }
+
+    generateDotFile(parent, graph);
+}
+
+void primMST(int graph[V][V]) {
+    int parent[V];
+    int key[V];
+    bool mstSet[V];
+
+    for (int i = 0; i < V; i++) {
+        key[i] = INT_MAX, mstSet[i] = false;
+    }
+
+    key[0] = 0;
+    parent[0] = -1;
+
+    for (int count = 0; count < V - 1; count++) {
+        int u = minKey(key, mstSet);
+        mstSet[u] = true;
+
+        for (int v = 0; v < V; v++) {
+            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v]) {
+                parent[v] = u, key[v] = graph[u][v];
+            }
+        }
+    }
+
+    printMST(parent, graph);
+}
 
 int main() {
+    /* Let us create the following graph
+       2    3
+    (0)--(1)--(2)
+     |   / \   |
+    6| 8/   \5 |7
+     | /     \ |
+    (3)-------(4)
+         9          */
 
+    int graph[V][V] = {
+            {0, 4, 0, 0, 0, 0, 0, 8, 0},
+            {4, 0, 8, 0, 0, 0, 0, 11, 0},
+            {0, 8, 0, 7, 0, 4, 0, 0, 2},
+            {0, 0, 7, 0, 9, 14, 0, 0, 0},
+            {0, 0, 0, 9, 0, 10, 0, 0, 0},
+            {0, 0, 4, 14, 10, 0, 2, 0, 0},
+            {0, 0, 0, 0, 0, 2, 0, 1, 6},
+            {8, 11, 0, 0, 0, 0, 1, 0, 7},
+            {0, 0, 2, 0, 0, 0, 6, 7, 0}
+    };
+
+    primMST(graph);
 
     return 0;
 }
